b65f5dfb5a31311e86b0c83dc4ff44a0dc61418e
Yahya Emre Gülmüş <70634956+Yegath@users.noreply.github.com>
2022-06-02 12:24:10 +0300

    Create Problem1.py

---
+++ Problem1.py
@@ -0,0 +1,16 @@
+# The algorithm that calculates the average of 5 entered numbers with *For Loop*
+
+t = 0
+i = 1
+
+while i < 6:
+    try:
+        s = int(input("Enter "+ str(i) + ".number: "))
+    except:
+        print("Attention! Please Enter a Number.")
+        continue
+    else:
+        t += s
+        i += 1
+a = t / (i - 1)
+print("the average of this five entered numbers is "+ str(a))
