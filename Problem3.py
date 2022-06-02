58f7cc7533fe7ea6d5095cf20231fc413ccce60b
Yahya Emre Gülmüş <70634956+Yegath@users.noreply.github.com>
2022-06-02 12:31:27 +0300

    Create Problem3.py

---
+++ Problem3.py
@@ -0,0 +1,28 @@
+# Find the average of the given undetermined number of integers by keyboard. Write a Python program that prints "The average is negative!" on the screen if the average is negative, and "The average is equal to zero!"on the screen if the average is equal to zero, and "The average is positive!" on the screen if the average is positive.
+
+i = 1
+s = 0
+
+while True:
+    n = int(input("Enter a number: "))
+    s += n
+    a = input("Is there another number? (Y/N): ")
+    if a == "N":
+        break
+    elif a == "Y":
+        i += 1
+    else:
+        print("Attention! Enter 'Y' for Yes, 'N' for No.")
+        a = input("Is there another number? (Y/N): ")
+
+if i != 0:
+    av = s / i
+    print("Average of the given undetermined number of integers is "+ str(av)+ ".")
+    if av < 0:
+        print("The average is negative!")
+    elif av == 0:
+        print("The average is equal to zero!")
+    else:
+        print("The average is positive!")
+else:
+    print("There is no given undetermined number of integers.")
